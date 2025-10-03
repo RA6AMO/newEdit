1f2d8e2465e1e7d6b55704f8c5191f1583d86fca
LEXXES <lexixes@gmail.com>
2025-10-03 21:07:48 +0300

    carefull work with tree widget

--- include/TreeWidgets/LTreeWidget.h
+++ include/TreeWidgets/LTreeWidget.h
@@ -1,7 +1,17 @@
 #pragma once
 #include <QTreeWidget>
 #include <QMainWindow>
+#include <QMenu>
+#include <QAction>
+#include <QInputDialog>
 #include "DBManager.h"
+#include <vector>
+#include <algorithm>
+#include <QMap>
+#include <memory>
+
+
+struct TreeStruct;
 
 class LTreeWidget : public QTreeWidget
 {
@@ -10,8 +20,16 @@ public:
     LTreeWidget(QString tableName,QTreeWidget *parent = nullptr, DatabaseManager *dbMan = nullptr);
     ~LTreeWidget();
 
+    void addNodeToRoot(const QString &node);
+    void addNodeToParent(const QString &node, const QString &parentId);
+
 private:
     void iniTree(QString tableName);
     QTreeWidget *parent;
     DatabaseManager *dbMan;
+
+    //std::vector<TreeStruct> treeNodes;
+    QMap<QString, std::shared_ptr<QTreeWidgetItem>> itemMap;
+
+
 };
